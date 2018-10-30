762292ef16adf8d1d9cf542a279e9996e9ed4836
adam <atomicpenguines@gmail.com>
2018-10-30 22:54:00 +0000

    basic jsx output

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component that produces HTML
+const App = () => {
+  return <div>hi</div>;
+};
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// put the component in the dom
+ReactDOM.render(<App />, document.querySelector(".container"));
