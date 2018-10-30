f866794a7d78114733a2f2bcb33ee47d2e024a6f
adam <atomicpenguines@gmail.com>
2018-10-30 23:55:55 +0000

    changed searchBar to a class based component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: "" };
+  }
   render() {
-    return <input type="text" />;
+    return (
+      <input type="text" onChange={event => console.log(event.target.value)} />
+    );
   }
 }
 
