78f8e6a879bc5be7f480ce6a130161651f47e18e
adam <atomicpenguines@gmail.com>
2018-10-30 23:43:21 +0000

    created component filess

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SearchBar from "./components/search_bar";
+
+const API_KEY = "AIzaSyBc80HauFB9dNK_pMBw3vewv3IehQyFp-s";
 
 // Create a new component that produces HTML
 const App = () => {
-  return <div>hi</div>;
+  return (
+    <div>
+      <h1>Hi</h1>
+      <SearchBar />
+    </div>
+  );
 };
 
 // put the component in the dom
