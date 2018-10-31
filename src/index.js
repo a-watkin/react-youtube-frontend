6b0fb056ce6bfc2327d1f537c20d540a20fd53f3
adam <atomicpenguines@gmail.com>
2018-10-31 01:01:02 +0000

    api call to put data in index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./components/search_bar";
 import key from "../API_KEY";
+import YTSearch from "youtube-api-search";
+// import SearchBar from "./components/search_bar";
 
 const API_KEY = key;
 
 // Create a new component that produces HTML
-const App = () => {
-  return (
-    <div>
-      <h1>Hi</h1>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: "penguins" }, videos => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hi</h1>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 // put the component in the dom
 ReactDOM.render(<App />, document.querySelector(".container"));
