ead8eef79a100b14677ee7196db5bce250ee5ea7
adam <atomicpenguines@gmail.com>
2018-10-31 00:18:46 +0000

    end of first section

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,13 +4,14 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: "" };
+    this.state = { term: "hello" };
   }
   render() {
     return (
       <div>
         <input
           type="text"
+          value={this.state.term}
           onChange={event => this.setState({ term: event.target.value })}
         />
         Value of input: {this.state.term}
